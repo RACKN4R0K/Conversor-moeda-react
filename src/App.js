f96b29fa57ae01c3dc5d729900104aea22c08f23
Henrique <henrique.hfr2006@gmail.com>
2021-12-30 18:15:06 -0300

    main

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <h2 className="Titulo">Convertor de moedas atualizado</h2>
       <Conversor moedaA="USD" moedaB="BRL"></Conversor>
-      <Conversor moedaA="BTC" moedaB="BRL"></Conversor>
       <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
     </div>
   );
